768dceb5054f259f2eada05818c4f622dabe554d
fpfhodor <fpfhodor@gmail.com>
2025-09-24 18:55:09 +0330

    Location.java added

---
+++ src/com/model/Location.java
@@ -0,0 +1,41 @@
+package com.model;
+
+import java.util.Objects;
+
+
+// Represents a location with x and y coordinates.
+
+public class Location {
+    private int x;
+    private int y;
+
+    public Location(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() { return x; }
+    public int getY() { return y; }
+
+    public double distanceTo(Location other) {
+        return Math.sqrt(Math.pow(this.x - other.x, 2) + Math.pow(this.y - other.y, 2));
+    }
+
+    @Override
+    public String toString() {
+        return "(" + x + ", " + y + ")";
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Location location = (Location) o;
+        return x == location.x && y == location.y;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(x, y);
+    }
+}
